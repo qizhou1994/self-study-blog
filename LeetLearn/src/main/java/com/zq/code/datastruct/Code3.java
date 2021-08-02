63e0f6bc940063ddc56c3ab92b78d1f159e6ac44
zhouqi <zhouqi@etyun.top>
2021-08-02 16:28:50 +0800

    day p

--- LeetLearn/src/main/java/com/zq/code/datastruct/Code3.java
+++ LeetLearn/src/main/java/com/zq/code/datastruct/Code3.java
@@ -1,5 +1,10 @@
 package com.zq.code.datastruct;
 
+import com.sun.org.apache.bcel.internal.classfile.Code;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * @author zhouqi
  * @desc
@@ -9,10 +14,29 @@ public class Code3 {
 
     public int lengthOfLongestSubstring(String s) {
 
-        int[] x = new int[256];
-        for(int i = 0 ; i < s.length();i++){
-            
+        int l = 0 ;
+        int r = 0;
+        int max = 0;
+        HashSet<Character> x = new HashSet<Character>();
+        for(int i = 0 ; i < s.length();i++) {
+            while (l<i&&x.contains(s.charAt(i))){
+                x.remove(s.charAt(l++));
+            }
+            if(!x.contains(s.charAt(i))){
+                r++;
+                x.add(s.charAt(i));
+            }
+            max = Math.max(max,r-l);
         }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Code3().lengthOfLongestSubstring("dvdf"));
+        System.out.println(new Code3().lengthOfLongestSubstring(""));
+        System.out.println(new Code3().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new Code3().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new Code3().lengthOfLongestSubstring("pwwkew"));
     }
 
 }
