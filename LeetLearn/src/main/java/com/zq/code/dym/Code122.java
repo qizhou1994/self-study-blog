42bdc0b2d7247933fdef0a7c9e68467a397ee3de
zhouqi <zhouqi@etyun.top>
2021-08-03 17:59:37 +0800

    day p  a

--- LeetLearn/src/main/java/com/zq/code/dym/Code122.java
+++ LeetLearn/src/main/java/com/zq/code/dym/Code122.java
@@ -6,16 +6,25 @@ package com.zq.code.dym;
  * @data 2021/7/29
  */
 public class Code122 {
+/*    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; ++i) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
+    }*/
     public int maxProfit(int[] prices) {
-
-        int max = 0;
-        int[][] x= new int[prices.length][prices.length];
-        for(int i = 0 ; i < prices.length;i++){
-        for(int j = 0 ; j < prices.length;j++) {
-            x[i][j] = prices[j]-prices[i];
+        int n = prices.length;
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            int newDp0 = Math.max(dp0, dp1 + prices[i]);
+            int newDp1 = Math.max(dp1, dp0 - prices[i]);
+            dp0 = newDp0;
+            dp1 = newDp1;
         }
-
-        }
-        return max;
+        return dp0;
     }
+
+
 }
